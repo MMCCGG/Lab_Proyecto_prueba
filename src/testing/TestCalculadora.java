42009cb8da828f410bbde44dbaf26bcd507a08e5
MMCCGG <marianocgarcia@hotmail.com>
2024-05-08 16:28:49 +0200

    el user1 ha añadido los casos de prueba al test de la calculadora

--- src/testing/TestCalculadora.java
+++ src/testing/TestCalculadora.java
@@ -1,5 +1,19 @@
 package testing;
 
-public class TestCalculadora {
+import dao.CalculadoraDao;
+import dao.CalculadoraDaoImpl;
 
-}
+public class TestCalculadora {
+	public static void main (String[] args) {
+		CalculadoraDao cdao = new CalculadoraDaoImpl();
+			
+		System.out.println(cdao.sumar(17,8));
+		System.out.println(cdao.restar(17,8));
+		System.out.println(cdao.multiplicar(17,8));
+		System.out.println(cdao.dividir(17,8));
+		System.out.println(cdao.resto(17,8));
+		
+			
+		}
+		
+	}
